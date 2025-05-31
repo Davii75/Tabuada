6d5bada5be0d36e3b4ff79f5e56173075c9fcd60
Davi de Castro Oliveira <davicastrooliveira@outlook.com>
2025-05-31 20:48:18 -0300

    Programa que mostra a tabuada de qualquer número que foi digitado

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿int NUMERO, MULTIPLICADOR, RESULTADO;
+
+Console.Write("Digite um número para gerar sua tabuada: ");
+NUMERO = Convert.ToInt32(Console.ReadLine());
+
+MULTIPLICADOR = 0;
+
+while (MULTIPLICADOR <= 10)
+{
+    RESULTADO = NUMERO * MULTIPLICADOR;
+    Console.WriteLine($"{NUMERO} x {MULTIPLICADOR} = {RESULTADO}");
+    MULTIPLICADOR += 1;
+}
